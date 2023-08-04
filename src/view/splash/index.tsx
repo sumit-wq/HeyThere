fad42c87af730bbc356f23d3db797672b7f88f22
Sumit Kumar Gupta <sumitkumargupta@Sumits-MacBook-Air.local>
2023-08-04 14:19:20 +0530

    splash screen edge cases handles

--- src/view/splash/index.tsx
+++ src/view/splash/index.tsx
@@ -1,96 +1,76 @@
 import React, {useEffect, useState} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Platform,
-  PermissionsAndroid,
-} from 'react-native';
+import {View, Text, StyleSheet, Alert, Linking} from 'react-native';
 import colors from '../../theme/defaultColor';
-import {useCurrentLocation} from '../../hooks/useLocationTracking';
+import {useLocationTracking} from '../../hooks/useLocationTracking';
 import {useNavigation} from '@react-navigation/native';
 import {SCREENS} from '../../navigations/navigationRoutes';
 import {ASYNC_ENUM} from '../../hooks/useAsyncStorage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {PERMISSIONS, check, request, RESULTS} from 'react-native-permissions';
 import firestore from '@react-native-firebase/firestore';
+import {PERMISSIONS, check, request, RESULTS} from 'react-native-permissions';
 
 const Splash = () => {
   const navigation = useNavigation();
   const [loadingError, setLoadingError] = useState(false);
 
-  const [location, locationError, getLocation] = useCurrentLocation();
+  const [location, locationError, permissionGranted, getLocation] =
+    useLocationTracking();
 
   const loadUserData = async () => {
     try {
       const data = await AsyncStorage.getItem(ASYNC_ENUM.X_USER_ID);
       if (!location && data) {
-        const userData = await firestore().collection('users').doc(data).get();
-        const {latitude, longitude} = userData._data;
-        await AsyncStorage.setItem(
-          ASYNC_ENUM.LATITUDE,
-          JSON.stringify(latitude),
-        );
-        await AsyncStorage.setItem(
-          ASYNC_ENUM.LONGITUDE,
-          JSON.stringify(longitude),
-        );
+        const userDataSnapshot = await firestore()
+          .collection('users')
+          .doc(data)
+          .get();
+        if (userDataSnapshot.exists) {
+          const userData = userDataSnapshot.data();
+          const {latitude, longitude} = userData;
+          await AsyncStorage.setItem(
+            ASYNC_ENUM.LATITUDE,
+            JSON.stringify(latitude),
+          );
+          await AsyncStorage.setItem(
+            ASYNC_ENUM.LONGITUDE,
+            JSON.stringify(longitude),
+          );
+        } else {
+          // Handle case where the document doesn't exist
+          Alert.alert('User data not found', 'Please try logging in again.');
+          setLoadingError(true);
+          return;
+        }
       }
 
-      const locationPermissionStatus = await checkLocationPermissionStatus();
-      navigation.navigate(
-        locationPermissionStatus ? SCREENS.MAP : SCREENS.LOGIN,
-        {location: location},
-      );
+      if (!permissionGranted) {
+        // Handle case where location permission is not granted
+        Alert.alert(
+          'Location Permission Required',
+          'To use this app, you need to enable location permission. Please go to Settings and allow location access.',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+              text: 'Open Settings',
+              onPress: () => {
+                Linking.openSettings();
+              },
+            },
+          ],
+          {cancelable: false},
+        );
+        setLoadingError(true);
+        return;
+      }
+
+      navigation.navigate(SCREENS.MAP);
     } catch (error) {
       // Handle or log the error
       console.warn('Error while loading user data:', error);
       setLoadingError(true);
-    }
-  };
-
-  const checkLocationPermissionStatus = async () => {
-    try {
-      const status = await check(
-        Platform.select({
-          android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-          ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
-        }),
-      );
-
-      if (status === RESULTS.GRANTED) {
-        return true;
-      } else if (status === RESULTS.DENIED) {
-        // Location permission is denied, request it
-        const permissionResult = await requestLocationPermission();
-        if (permissionResult === RESULTS.GRANTED) {
-          return true;
-        } else {
-          return false;
-        }
-      } else if (status === RESULTS.BLOCKED) {
-        // Location permission is blocked, handle it accordingly
-        setLoadingError(true);
-        return false;
-      }
-    } catch (error) {
-      // Handle or log the error
-      console.warn('Error while checking location permission status:', error);
-      setLoadingError(true);
-      return false; // Return a default value or handle the error state accordingly
-    }
-  };
-
-  const requestLocationPermission = async () => {
-    try {
-      const result = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      );
-      return result;
-    } catch (error) {
-      // Handle or log the error
-      console.warn('Error while requesting location permission:', error);
-      return RESULTS.DENIED;
     }
   };
 
